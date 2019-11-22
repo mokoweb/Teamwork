677073b4008e41ce32fcabfd01a53283eff5d9f5
Olaolu Amoko <odamoko@sidmach.com>
2019-11-22 10:53:43 +0100

    bg-fix-database-model-issues

--- db.js
+++ db.js
@@ -14,35 +14,84 @@ pool.on('connect', () => {
 /**
  * Create Tables
  */
-const createTables = () =>{
-    //createUsersTables()
-    //createGifsTables()
-     //createArticlesTables()
-    // createCommentsPostTables()
-     //createCommentsGifTables()
-    // createCategoryTables()
-    //createSharedTables
-    createInappropriateTables()
-    
-    pool.end();
-}
+  async function createTables() {
+ const queryText =`
+ CREATE TABLE IF NOT EXISTS
+       users(
+         userId INTEGER PRIMARY KEY,
+         firstName VARCHAR(128) NOT NULL,
+         lastName VARCHAR(128) NOT NULL,
+         email VARCHAR(128) NOT NULL,
+         password VARCHAR(128)  NOT NULL,
+         gender VARCHAR(128)  NOT NULL,
+         jobRole VARCHAR(128)  NOT NULL,
+         isAdmin bool DEFAULT false,
+         address VARCHAR(128)  NOT NULL,
+         department VARCHAR(128)  NOT NULL,
+         createdOn TIMESTAMP
+       );
+ 
+  
+       CREATE TABLE IF NOT EXISTS
+         gifs(
+           gifId INTEGER PRIMARY KEY,
+           title VARCHAR(128) NOT NULL,
+           postedBy INTEGER NOT NULL,
+           imageUrl VARCHAR(255) NOT NULL,
+           inAppropriate bool DEFAULT false,
+           createdOn TIMESTAMP
+         );
+   
+ 
+       CREATE TABLE IF NOT EXISTS
+         articles(
+           articleId INTEGER PRIMARY KEY,
+           title VARCHAR(128) NOT NULL,
+           postedBy INTEGER NOT NULL,
+           article TEXT NOT NULL,
+           inAppropriate bool DEFAULT false,
+           createdOn TIMESTAMP
+         );
+   
+ 
+       CREATE TABLE IF NOT EXISTS
+       commentsPost(
+         commentId INTEGER PRIMARY KEY,
+         articleId INTEGER  NOT NULL,
+         postedBy INTEGER NOT NULL,
+         comments TEXT NOT NULL,
+         inAppropriate bool DEFAULT false,
+         createdOn TIMESTAMP
+         );
+   
+ 
+       CREATE TABLE IF NOT EXISTS
+       commentsGif(
+         id INTEGER PRIMARY KEY,
+         gifId INTEGER  NOT NULL,
+         postedBy INTEGER NOT NULL,
+         comments TEXT NOT NULL,
+         inAppropriate bool DEFAULT false,
+         createdOn TIMESTAMP
+         );
+ 
+       CREATE TABLE IF NOT EXISTS
+         category(
+         categoryId INTEGER PRIMARY KEY,
+         title VARCHAR(255) NOT NULL,
+         createdOn TIMESTAMP
+         );
+   
+       CREATE TABLE IF NOT EXISTS
+       articlesShared(
+       articlesSharedId INTEGER PRIMARY KEY,
+       articleId INTEGER  NOT NULL,
+       sharedBy INTEGER NOT NULL,
+       sharedWith INTEGER NOT NULL,
+       createdOn TIMESTAMP
+       );`
 
-const createUsersTables = () => {
-
-  const queryText =
-    `CREATE TABLE IF NOT EXISTS
-      users(
-        id UUID PRIMARY KEY,
-        name VARCHAR(128) NOT NULL,
-        email VARCHAR(128) NOT NULL,
-        level SMALLINT NOT NULL,
-        isAdmin bool NULL,
-        isActive bool NULL,
-        createdDate DATE,
-        modifiedDate TIMESTAMP
-      )`;
-
-  pool.query(queryText)
+    pool.query(queryText)
     .then((res) => {
       console.log(res);
       pool.end();
@@ -53,214 +102,15 @@ const createUsersTables = () => {
     });
 }
 
-
-const createGifsTables = () => {
-    const queryText =
-      `CREATE TABLE IF NOT EXISTS
-        gifs(
-          id UUID PRIMARY KEY,
-          name VARCHAR(128) NOT NULL,
-          postedBy UUID NOT NULL,
-          gif TEXT NOT NULL,
-          inAppropriate bool NULL,
-          isActive bool NULL,
-          createdDate DATE,
-        modifiedDate TIMESTAMP
-        )`;
-  
-    pool.query(queryText)
-      .then((res) => {
-        console.log(res);
-        pool.end();
-      })
-      .catch((err) => {
-        console.log(err);
-        pool.end();
-      });
-  }
-
-  const createArticlesTables = () => {
-    const queryText =
-      `CREATE TABLE IF NOT EXISTS
-        articles(
-          id UUID PRIMARY KEY,
-          name VARCHAR(128) NOT NULL,
-          postedBy UUID NOT NULL,
-          article TEXT NOT NULL,
-          inAppropriate bool NULL,
-          isActive bool NULL,
-          createdDate DATE,
-        modifiedDate TIMESTAMP
-        )`;
-  
-    pool.query(queryText)
-      .then((res) => {
-        console.log(res);
-        
-      })
-      .catch((err) => {
-        console.log(err);
-        
-      });
-  }
-
-  const createCommentsPostTables = () => {
-    const queryText =
-      `CREATE TABLE IF NOT EXISTS
-      commentsPost(
-        id UUID PRIMARY KEY,
-        articleId UUID  NOT NULL,
-        postedBy UUID NOT NULL,
-        comments TEXT NOT NULL,
-        createdDate DATETIME,
-        modifiedDate TIMESTAMP
-        )`;
-  
-    pool.query(queryText)
-      .then((res) => {
-        console.log(res);
-        
-      })
-      .catch((err) => {
-        console.log(err);
-        
-      });
-  }
-
-  const createCommentsGifTables = () => {
-    const queryText =
-      `CREATE TABLE IF NOT EXISTS
-      commentsPost(
-        id UUID PRIMARY KEY,
-        gifId UUID  NOT NULL,
-        postedBy UUID NOT NULL,
-        comments TEXT NOT NULL,
-        createdDate DATE,
-        modifiedDate TIMESTAMP
-        )`;
-  
-    pool.query(queryText)
-      .then((res) => {
-        console.log(res);
-        
-      })
-      .catch((err) => {
-        console.log(err);
-        
-      });
-  }
-
-  const createCategoryTables = () => {
-    const queryText =
-      `CREATE TABLE IF NOT EXISTS
-        category(
-        id UUID PRIMARY KEY,
-        categoryId UUID  NOT NULL,
-        postedBy UUID NOT NULL,
-        name VARCHAR(255) NOT NULL,
-        createdDate DATE,
-        modifiedDate TIMESTAMP
-        )`;
-  
-    pool.query(queryText)
-      .then((res) => {
-        console.log(res);
-        
-      })
-      .catch((err) => {
-        console.log(err);
-        
-      });
-  }
-
-  const createArticlesSharedTables = () => {
-     const queryText =
-    `CREATE TABLE IF NOT EXISTS
-      articlesShared(
-      id UUID PRIMARY KEY,
-      articleId UUID  NOT NULL,
-      sharedBy UUID NOT NULL,
-      sharedWith UUID NOT NULL,
-      comments VARCHAR(255),
-      createdDate DATETIME,
-      modifiedDate TIMESTAMP
-      )`;
-  
-    pool.query(queryText)
-      .then((res) => {
-        console.log(res);
-        
-      })
-      .catch((err) => {
-        console.log(err);
-        
-      });
-  }
-  const createGifsSharedTables = () => {
-     const queryText =
-    `CREATE TABLE IF NOT EXISTS
-      articlesShared(
-      id UUID PRIMARY KEY,
-      articleId UUID  NOT NULL,
-      sharedBy UUID NOT NULL,
-      sharedWith UUID NOT NULL,
-      comments VARCHAR(255),
-      createdDate DATETIME,
-      modifiedDate TIMESTAMP
-      )`;
-  
-    pool.query(queryText)
-      .then((res) => {
-        console.log(res);
-        
-      })
-      .catch((err) => {
-        console.log(err);
-        
-      });
-  }
-  const createInappropriateTables = () => {
-    const queryText =
-      `CREATE TABLE IF NOT EXISTS
-        Inappropriate(
-        id UUID PRIMARY KEY,
-        postId UUID  NOT NULL,
-        postType VARCHAR(10),
-        postedBy UUID NOT NULL,
-        comments VARCHAR(255),
-        createdDate DATETIME,
-        modifiedDate TIMESTAMP
-        )`;
-  
-    pool.query(queryText)
-      .then((res) => {
-        console.log(res);
-        
-      })
-      .catch((err) => {
-        console.log(err);
-        
-      });
-  }
-
-
+   
 
 /**
  * Drop Tables
  */
 const dropTables = (tableName) => {
   const queryText = 'DROP TABLE IF EXISTS '+tableName;
-  pool.query(queryText)
-    .then((res) => {
-      console.log(res);
-      pool.end();
-    })
-    .catch((err) => {
-      console.log(err);
-      pool.end();
-    });
+ 
 }
-
 pool.on('remove', () => {
   console.log('client removed');
   process.exit(0);
