36f879372f5e6972e266030d1b7b60b906aa0177
Olaolu Amoko <odamoko@sidmach.com>
2019-11-23 13:13:35 +0100

    ft-create-api-to-add-users

--- app.js
+++ app.js
@@ -19,8 +19,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
-
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -28,7 +26,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/api/v2/users', usersRouter);
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
